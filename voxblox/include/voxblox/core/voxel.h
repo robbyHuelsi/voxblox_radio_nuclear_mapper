63698f96ddd428e0b738b2257e1db1676522d83c
Robert Hülsmann <robert@huelsi.de>
2020-02-04 14:27:40 +0100

    unrelated file voxel.h reserved

--- voxblox/include/voxblox/core/voxel.h
+++ voxblox/include/voxblox/core/voxel.h
@@ -43,7 +43,7 @@ struct OccupancyVoxel {
 
 struct IntensityVoxel {
   float intensity = 0.0f;
-  float weight = std::numeric_limits<float>::infinity();
+  float weight = 0.0f;
 };
 
 /// Used for serialization only.
